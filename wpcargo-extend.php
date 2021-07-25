c63561c66e133c268b08aa07c205cd5ed6af5b7a
PUYUP <hellopuyup@gmail.com>
2021-07-26 06:07:34 +0700

    round

--- wpcargo-extend.php
+++ wpcargo-extend.php
@@ -19,6 +19,10 @@ function footer_scripts() { ?>
     <script type="text/javascript">
         (function ($) {
             $(document).ready(function () {
+                var inputLength = 10;
+                var inputWidth = 15;
+                var inputHeight = 5;
+
                 $(document).on('keyup', '#wpcfe-packages-repeater input', function(value) {
                     var this$ = $(this);
                     var tr$ = this$.closest('tr');
@@ -29,11 +33,11 @@ function footer_scripts() { ?>
 
                     // Volume
                     var volume = (inputLength * inputWidth * inputHeight) / 4000;
-                    tr$.find("input[name*='[tvolume]']").val(Math.floor(volume* 1000) / 1000);
+                    tr$.find("input[name*='[tvolume]']").val(Math.round(volume* 10) / 10);
                     
                     // Cubic
                     var cubic = (inputLength * inputWidth * inputHeight) / 1000000;
-                    tr$.find("input[name*='[tkubikasi]']").val(Math.floor(cubic* 1000) / 1000);
+                    tr$.find("input[name*='[tkubikasi]']").val(Math.round(cubic* 1000) / 1000);
                 });
             });
         })(jQuery);
